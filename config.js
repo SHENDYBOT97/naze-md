404491bdc43da7503959d4de4a10424bf5139291
SHENDYBOT97 <117246778+SHENDYBOT97@users.noreply.github.com>
2022-11-02 10:01:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
+global.owner = ['6283861760786']
 global.ownernomer = "6285875158363"
 global.premium = ['6285875158363']
 global.packname = 'Sticker By'
